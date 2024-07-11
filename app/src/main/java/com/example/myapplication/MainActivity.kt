7df11166082865adbceaab7a95ffcd3be12e2055
shubhamKumar <appscicseshubham22@gmail.com>
2024-07-11 21:59:04 +0530

    button and toast
    on start, on resume

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    var btn: Button?=null//var declaration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Toast.makeText(this,"onClick",Toast.LENGTH_SHORT).show()
+        btn=findViewById(R.id.button)
+
+        btn?.setOnClickListener{
+            Toast.makeText(this,"saved",Toast.LENGTH_SHORT).show()
+        }
     }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this,"onStart",Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this,"onResume",Toast.LENGTH_SHORT).show()
+    }
+    
+
 }
